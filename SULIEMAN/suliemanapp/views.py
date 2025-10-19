402ec0ccc6c0c0ea7e1168ae3bf2d280ed585384
salhmlan880-lang <salhmlan880@gmail.com>
2025-10-19 13:33:52 +0300

    Update views.py

--- SULIEMAN/suliemanapp/views.py
+++ SULIEMAN/suliemanapp/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def sulieman(request):
-    return HttpResponse("")
+ 
+    return HttpResponse("""
+  <html>
+    <body>
+      <h1>sulieman </h1>
+      <p>2211112510 <strong>html</strong> web app!</p>
+    </body>
+  </html>
+  """)
